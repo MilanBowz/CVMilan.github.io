90b379df99713a707c886d89712e8d351c1ffe98
Private cap <91745418+MilanBowz@users.noreply.github.com>
2023-02-26 17:38:17 +0100

    spinwheel

--- mail.php
+++ mail.php
@@ -1,19 +1,4 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-
-$to = "milan.posman2002@gmail.com";
-$subject = "CV-mail Milan";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: CV-Milan" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:index.html");
+$myText = $_POST["mytext"];
+file_put_contents("test.txt", $myText);
 ?>
